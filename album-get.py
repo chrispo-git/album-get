25f9ac0ba89464d14132389447d331075e083969
chrispo <haasc865@gmail.com>
2025-09-03 10:11:31 +0100

    Update album-get.py

--- album-get.py
+++ album-get.py
@@ -1,14 +1,13 @@
 import requests
 import json
-
-def getAlbumJson(album: str, artist: str):
+headers = {
+    "User-Agent": "album-get/0.1.0 (chaastrup865@gmail.com)"
+}
+def getAlbumID(album: str, artist: str):
     album_link = f'https://musicbrainz.org/ws/2/release/?query=release:"{album}" AND artist:"{artist}"&fmt=json'
-    album_info = requests.get(album_link).json()
+    album_info = requests.get(album_link, headers=headers).json()
     release_id = album_info["releases"][0]["id"]
-    print(release_id)
-    return 
+    return release_id
 
 
-getAlbumJson("The College Dropout", "Kanye West")
-
-#print(getAlbumJson("The College Dropout", "Kanye West"))
+print(getAlbumID("The College Dropout", "Kanye West"))

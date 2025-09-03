b3488335b0871354ee78f8c1eb49f8982c7c5760
chrispo <haasc865@gmail.com>
2025-09-03 21:06:50 +0100

    Package Setup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # album-get
  Gets an album from a given artist name and album name
+
+## Installation
+1. Run `pip install git+https://github.com/chrispo-git/album-get`
+2. Install requirements using `pip install -r requirements.txt`
--- albumGet/main.py
+++ albumGet/main.py
@@ -5,6 +5,7 @@ import os
 import sys
 import yt_dlp
 import time
+import argparse
 import shutil
 from datetime import datetime, timedelta
 from mutagen.mp3 import MP3
@@ -58,44 +59,44 @@ def printTracklist(metadata):
     time.sleep(1)
     data = getAlbumData(metadata["id"])
     tracklist = parseTracks(data)
-    totalLength = len(meta["artist-credit"][0]["name"])+ len(meta["title"]) + len("   ")
+    totalLength = len(metadata["artist-credit"][0]["name"])+ len(metadata["title"]) + len("   ")
     for i in tracklist:
         if len(i['title'])+9 > totalLength:
             totalLength = len(i['title'])+15
     print("\n")
-    print(middleSpace(meta["artist-credit"][0]["name"], meta["title"], totalLength))
+    print(middleSpace(metadata["artist-credit"][0]["name"], metadata["title"], totalLength))
     print("")
     print(f"#  Title{getSpace("",totalLength-14)}Length")
     for i in tracklist:
         print(middleSpace(f"{i['position']}{getSpace(i['position'],3)}{i['title']}", i['length'], totalLength))
-def downloadTrackList(metadata):
-    if not os.path.isdir("output"):
-        os.mkdir("output")
+def downloadTrackList(metadata, output_folder):
+    if not os.path.isdir(f"{output_folder}"):
+        os.mkdir(f"{output_folder}")
     else:
-        shutil.rmtree("output")
-        os.mkdir("output")
+        shutil.rmtree(f"{output_folder}")
+        os.mkdir(f"{output_folder}")
     print("Downloading Album Cover...")
     cover = requests.get(f"https://coverartarchive.org/release/{metadata["id"]}/front").content
-    with open('output/cover.jpg', 'wb') as img:
+    with open(f'{output_folder}/cover.jpg', 'wb') as img:
         img.write(cover)
     img.close()
     time.sleep(1)
     data = getAlbumData(metadata["id"])
     tracklist = parseTracks(data)
     for i in tracklist:
-        downloadAudio(f"{i['title']} - {meta["artist-credit"][0]["name"]}", i['length'])
+        downloadAudio(f"{i['title']} - {metadata["artist-credit"][0]["name"]}", i['length'], output_folder)
 
-def tagTracklist(metadata):
+def tagTracklist(metadata, output_folder):
     print("Tagging...")
     time.sleep(1)
     data = getAlbumData(metadata["id"])
     tracklist = parseTracks(data)
     
     for i in tracklist:
-        query = f"{i['title']} - {meta["artist-credit"][0]["name"]}"
+        query = f"{i['title']} - {metadata["artist-credit"][0]["name"]}"
         if "/" in query:
             query = query.replace("/", "∕")
-        audio = MP3(f"output/{query}.mp3",ID3=ID3)
+        audio = MP3(f"{output_folder}/{query}.mp3",ID3=ID3)
         audio.pprint()
         audio.tags.add(
             APIC(
@@ -103,23 +104,23 @@ def tagTracklist(metadata):
                 mime='image/jpg',
                 type=3,
                 desc=u'Cover',
-                data=open('output/cover.jpg', 'rb').read()
+                data=open(f'{output_folder}/cover.jpg', 'rb').read()
             )
         )
         audio.save()
-        audio = ID3(f"output/{query}.mp3")
+        audio = ID3(f"{output_folder}/{query}.mp3")
         audio.add(TIT2(encoding=3, text=u""+i['title']))    #TITLE
         audio.add(TRCK(encoding=3, text=u""+i['position']))    #TRACK
-        audio.add(TPE1(encoding=3, text=u""+meta["artist-credit"][0]["name"]))    #ARTIST
-        audio.add(TALB(encoding=3, text=u""+meta["title"]))   #ALBUM
-        audio.add(TYER(encoding=3, text=u""+meta["date"])) #YEAR
-        audio.add(TDAT(encoding=3, text=u""+meta["date"])) #YEAR
-        audio.add(TORY(encoding=3, text=u""+meta["date"])) #ORIGYEAR
-        audio.add(TPE2(encoding=3, text=u""+meta["artist-credit"][0]["name"]))   #ALBUMARTIST
+        audio.add(TPE1(encoding=3, text=u""+metadata["artist-credit"][0]["name"]))    #ARTIST
+        audio.add(TALB(encoding=3, text=u""+metadata["title"]))   #ALBUM
+        audio.add(TYER(encoding=3, text=u""+metadata["date"])) #YEAR
+        audio.add(TDAT(encoding=3, text=u""+metadata["date"])) #YEAR
+        audio.add(TORY(encoding=3, text=u""+metadata["date"])) #ORIGYEAR
+        audio.add(TPE2(encoding=3, text=u""+metadata["artist-credit"][0]["name"]))   #ALBUMARTIST
         audio.add(TCON(encoding=3, text=u""))    #GENRE
         audio.save(v2_version=3)
 
-def downloadAudio(query, desiredLength):
+def downloadAudio(query, desiredLength, output_folder):
     print(f"Checking Youtube For '{query}'...")
     if desiredLength[0] == "0" and len(desiredLength) == 5:
         desiredLength = desiredLength[1:]
@@ -134,15 +135,12 @@ def downloadAudio(query, desiredLength):
         finalURL = text[0].replace("\n","")
         for i in range(0,len(text)-1, 2):
             songCandidates.append([text[i].replace("\n",""), text[i+1].replace("\n","")])
-        print(desiredLength)
-        print(songCandidates)
         for i in songCandidates:
             try:
                 t1 = time.mktime(time.strptime(i[1], "%M:%S"))
                 desiredTime = time.mktime(time.strptime(desiredLength, "%M:%S"))
             except ValueError:
                 continue
-            print(desiredTime-t1)
             if desiredTime - t1 < 4 and t1 - desiredTime < 4:
                 finalURL = i[0]
                 replaced = True 
@@ -155,31 +153,44 @@ def downloadAudio(query, desiredLength):
                 print(f"Searching first {searchNum} results...")
     if "/" in query:
         query = query.replace("/", "∕")
-    os.system(f'yt-dlp -x --audio-format mp3 -o "output/{query}.mp3" https://www.youtube.com/watch?v={finalURL}')
+    os.system(f'yt-dlp -x --audio-format mp3 -o "{output_folder}/{query}.mp3" https://www.youtube.com/watch?v={finalURL}')
 
 
-album = input("Album Title:")
-artist = input("Artist:")
-query = AlbumQuery(album, artist)
-if len(query) < 1:
-    print("No Albums Found.")
-    time.sleep(1)
-    sys.exit()
-entry = 0
-meta = getAlbumMeta(query, 0)
-while True:
-    if entry >= len(query)-1:
-        entry = 0
-    meta = getAlbumMeta(query, entry)
-    printTracklist(meta)
-    exit = input("Is this the correct album? [y/n] ")
-    if exit.lower() == "y":
-        break
-    entry += 1
 
-start = time.time()
-downloadTrackList(meta)
-tagTracklist(meta)
-end = time.time()
-print(f"Finished in {int(end-start)//60}m {int(end-start)%60}s")
-print("Done! :)")
+
+def cli():
+    parser = argparse.ArgumentParser(
+        description="Download album tracks via MusicBrainz + YouTube"
+    )
+    parser.add_argument("artist", help="Artist name")
+    parser.add_argument("album", help="Album name")
+    parser.add_argument(
+        "-o", "--output", default=os.path.join(os.getcwd(), "album-out"),
+        help="Save Directory"
+    )
+    args = parser.parse_args()
+    album = args.album
+    artist = args.artist
+    query = AlbumQuery(album, artist)
+    output_folder = args.output
+    if len(query) < 1:
+        print("No Albums Found.")
+        time.sleep(1)
+        sys.exit()
+    entry = 0
+    meta = getAlbumMeta(query, 0)
+    while True:
+        if entry >= len(query)-1:
+            entry = 0
+        meta = getAlbumMeta(query, entry)
+        printTracklist(meta)
+        exit = input("Is this the correct album? [y/n] ")
+        if exit.lower() == "y":
+            break
+        entry += 1
+    start = time.time()
+    downloadTrackList(meta, output_folder)
+    tagTracklist(meta, output_folder)
+    end = time.time()
+    print(f"Finished in {int(end-start)//60}m {int(end-start)%60}s")
+    print("Done! :)")
